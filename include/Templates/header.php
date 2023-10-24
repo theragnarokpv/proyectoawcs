bf8373acd2125e6017ad8a719d3070d3e065e1d9
theragnarokpv <bolanosa68@gmail.com>
2023-10-24 14:08:10 -0600

    base para el proyecto

---
+++ include/Templates/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <div class="top-bar">
+            <div class="container">
+            <div class="logo-container">
+                <img src="img/casa-inteligente.png" alt="Logo" class="logo">
+                <h1 class="store-name">Tecno-Stream</h1>
+            </div>
+            <div class="other-logos">
+                <a href="busqueda.html"><img src="img/buscar.png" alt="Busqueda" class="other-logo"></a>
+                <a href="carrito.html"><img src="img/carrito-de-compras (1).png" alt="Compras" class="other-logo"></a>
+                <a href="usuario.html"><img src="img/perfil-del-usuario.png" alt="Usuario" class="other-logo"></a>
+            </div>
+            </div>
+        </div>
+
+        <div class="sidebar">
+            <div class="sidebar-logos">
+            <a href="monitores.html"><img src="img/vr.png" alt="Monitores" class="sidebar-logo"></a>
+            <a href="audifonos.html"><img src="img/auriculares.png" alt="Audifonos" class="sidebar-logo"></a>
+            <a href="teclados.html"><img src="img/teclado.png" alt="Teclados" class="sidebar-logo"></a>
+            <a href="mouse.html"><img src="img/raton-inalambrico.png" alt="Mouse" class="sidebar-logo"></a>
+            <a href="controles.html"><img src="img/almohadilla-de-juego (1).png" alt="Controles" class="sidebar-logo"></a>
+            <a href="camaras.html"><img src="img/camara.png" alt="Camaras" class="sidebar-logo"></a>
+            <a href="otros.html"><img src="img/zumbido.png" alt="Otros..." class="sidebar-logo"></a>
+            </div>
+        </div>
+    </header>    
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tecno-Stream</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="img/casa-inteligente.ico" type="image/x-icon">
+</head>
+<body>
+     <?php
+        include "include/Templates/header.php";
+     ?>
+</body>
+</html>
