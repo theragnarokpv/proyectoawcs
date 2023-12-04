5b6978f86fddafa3e3adfa51ecb7672d399d41ed
AGiart <117540143+AGiart@users.noreply.github.com>
2023-12-03 18:40:52 -0600

    cambios de adaptabilidad (incompleto)

--- include/Templates/header.php
+++ include/Templates/header.php
@@ -16,19 +16,44 @@
     <header>
         <div class="top-bar">
             <div class="container">
-            <a href="index.php">
-                <div class="logo-container">
-                    <img src="img/casa-inteligente.png" alt="Logo" class="logo">
-                    <h1 class="store-name">TIENDA VALHALLA</h1>
+                <a  href="index.php">
+                    <div class="logo-container">
+                        <img  src="img/casa-inteligente.png" alt="Logo" class="logo">
+                        <h1  class="store-name">TIENDA VALHALLA</h1>
+                    </div>
+                </a>
+                <div class="other-logos">
+                    <a href="carrito.php"><img src="img/carrito-de-compras (1).png" alt="Compras" class="other-logo"></a>
+
+                    <?php if (isset($_SESSION['id_usuario'])) : ?>
+                    <div class="dropdown">
+                        <button class="btn dropdown-toggle" id="btn_usuario" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="img/perfil-del-usuario.png" alt="Usuario" class="other-logo">
+                        </button>
+                        <ul class="dropdown-menu dropdown-menu-dark" id="drop">
+                        <?php
+                        // Verificar si el id_rol del usuario es igual a 3
+                            if ($id_rol_usuario <= 2) {
+                                echo '<li><a class="dropdown-item" href="adminIndex.php">Administración</a></li>';
+                            }
+                        ?>
+                            <li><a class="dropdown-item" href="perfil.php">Perfil</a></li>
+                            <li><a class="dropdown-item" href="pedidos.php">Pedidos</a></li>
+                            <li><a class="dropdown-item" href="?cerrar_sesion=1">Cerrar sesión</a></li>
+                        </ul>
+                    </div>
+                    <?php else : ?>
+                        <!-- Si el usuario no ha iniciado sesión, redirigir a la página de inicio de sesión -->
+                        <a href="inicioSesion.php" class="btn">
+                            <img src="img/perfil-del-usuario.png" alt="Usuario" class="other-logo">
+                        </a>
+                    <?php endif; ?>
                 </div>
             </a>
-            <div class="other-logos">
-                <a href="busqueda.html"><img src="img/buscar.png" alt="Busqueda" class="other-logo"></a>
-                <a href="carrito.php"><img src="img/carrito-de-compras (1).png" alt="Compras" class="other-logo"></a>
-                <a href="usuario.php"><img src="img/perfil-del-usuario.png" alt="Usuario" class="other-logo"></a>
-            </div>
+            
             </div>
         </div>
+        </header> 
 
 </body>
 </html>
--- index.php
+++ index.php
@@ -21,21 +21,42 @@
         include "include/Templates/nav.php";
     ?>
 
+    <div class="btn-mas" >
+         <button type='button' id="boton_buscar" class='btn btn-mas-icon' data-bs-toggle='modal' data-bs-target='#filtrarproductos'><img src="img/buscar.png" alt="Busqueda" class="other-logo"></button>
+    </div>
+
+                <div ID="pnlMensaje" title="Error" style="display:none">
+                    <div>
+                        <strong>Atención!</strong> Se ha presentado el siguiente problema.
+                        <br />
+                        <br />
+                        <p ID="blMensajes"></p>
+                    </div>
+                </div>
+                <div ID="pnlInfo" title="Mensaje" style="display : none;">
+                    <div>
+                        <strong>Información!</strong> Procesamiento éxitoso.
+                        <br />
+                        <br />
+                        <p ID="blInfo"></p>
+                    </div>
+                </div>
+
     <section class="main-content">
-        <div class="container">
-            <div class="row">
+        <div class="container ">
+            <div class="row aco">
                 <?php 
                     $resultado = $conn -> query("SELECT * FROM producto");
 
                     $datos = $resultado->fetch_assoc();
 
                     while ($datos != null){
-                        echo "<div class='col-md-3'>";
+                        echo "<div class='col'>";
                             echo "<div class='product'>";
                                 echo "<a href='producto.php?codigo={$datos['id_producto']}'><img src='{$datos['ruta_imagen']}' alt='{$datos['descripcion']}' class='product-image'></a>";
                                 echo "<h3 class='product-name'>{$datos['descripcion']}</h3>";
                                 echo "<h3 class='product-price'>₡ {$datos['precio']}</h3>";
-                                echo"<a href='include/functions/agregarCarrito.php?codigo={$datos['id_producto']}'><button class='btn_compra' ><i class='bi bi-cart-plus-fill'></i>AÑADIR</button></a>";
+                                echo"<a href='include/functions/agregarCarrito.php?codigo={$datos['id_producto']}'><button class='btn_compra bi bi-cart-plus-fill' > AÑADIR</button></a>";
                             echo"</div>";
                         echo"</div>";
 
